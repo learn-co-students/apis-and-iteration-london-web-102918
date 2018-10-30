f4f2f07dacc7646dfe3b7c974e191e0b1e7003e7
seeduardo <romenflatiron@gmail.com>
2018-10-30 18:37:30 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,11 +3,6 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-def run
-  welcome
-  character = get_character_from_user
-  show_character_movies(character)
-  run
-end
-
-run
+welcome
+character = get_character_from_user
+show_character_movies(character)
