a8e3939c7238cd499f24be9d9eb4dad60330e614
seeduardo <romenflatiron@gmail.com>
2018-10-30 15:36:00 +0000

    Add Welcome Method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,10 @@
+require "pry"
+
 def welcome
-  # puts out a welcome message here!
+  puts "Hello! Please enter a Star Wars character."
 end
+
+welcome
 
 def get_character_from_user
   puts "please enter a character"
