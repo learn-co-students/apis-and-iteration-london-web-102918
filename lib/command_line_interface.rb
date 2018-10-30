3a63ac692ab167572c4ef80b9fbdfda23ccf5bb8
kbberker <kbbuk90@gmail.com>
2018-10-30 15:57:25 +0000

    defined get_character_from_user method in command_line_interface.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,12 +1,12 @@
 require "pry"
 
 def welcome
-  puts "Hello! Please enter a Star Wars character."
+  puts "Hello! Welcome to Star Wars Character Wooki."
 end
 
-welcome
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character"
+  gets.chomp.downcase
 end
+
