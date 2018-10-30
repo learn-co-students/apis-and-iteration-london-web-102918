6d965a88632d7259ac97508fb173b11d434b11f0
github username <github email address>
2018-10-30 16:59:34 +0000

    can get character

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,13 +2,28 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
 def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
+  response = []
+
+  response_hash["results"].each do |char_hash|
+    if char_hash["name"].downcase.include?(character)
+      puts "Found #{char_hash["name"]}!"
+      puts "Getting film info!"
+      char_hash["films"].each do |link|
+         film_info = RestClient.get(link)
+         response << JSON.parse(film_info)
+       end
+    end
+  end
+  return response
+end
+
+
+
 
 
   # iterate over the response hash to find the collection of `films` for the given
@@ -20,16 +35,18 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
+
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  puts films_hash
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
+
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
 def welcome
-  # puts out a welcome message here!
+   puts "Welcome to Api and iteration lab"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "please enter a character: "
+  user_input = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
+  if user_input != String
+    user_input.to_s
+  end
 end
